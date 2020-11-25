334a73a5d850bbece4fc38801ed460e48e7b7f9d
andyshafer <andyshafer0715@gmail.com>
2020-11-25 00:46:00 -0500

    Added bindings for Path

---
+++ dypf/main/src/web-bindings/PathBinding.cpp
@@ -0,0 +1,14 @@
+#include <emscripten/bind.h>
+#include "../Path.h"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(path_binding) {
+	class_<Path>("Path")
+		.constructor<const std::vector<PathSegment>&>()
+		.constructor<>()
+		.function("addSegment", &Path::addSegment)
+		.function("getPos", &Path::getPos)
+		.function("getArriveTime", &Path::getArriveTime)
+		.function("getSegments", &Path::getSegments);
+}
