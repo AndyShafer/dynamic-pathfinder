0462512b87d705058cc8b7d97a9875bf60db7782
andyshafer <andyshafer0715@gmail.com>
2020-11-25 00:14:50 -0500

    Added more constructors and functions to PointBinding

--- dypf/main/src/web-bindings/PointBinding.cpp
+++ dypf/main/src/web-bindings/PointBinding.cpp
@@ -3,15 +3,18 @@
 #include "../Point.h"
 
 typedef Vec2f (Point::*PointGetPos1)() const;
+typedef Vec2f (Point::*PointGetPos2)(float) const;
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(point_binding) {
 	//Vec2f (*getPosFP1)() const = &Point::getPos;
 	class_<Point>("Point")
-		//.constructor<Vec2f>()
-		//.constructor<Vec2f, Vec2f>()
+		.constructor<Vec2f>()
+		.constructor<Vec2f, Vec2f>()
 		.constructor<float, float, float, float>()
 		.function("getPos", (PointGetPos1)&Point::getPos)
-		.function("getVelocity", &Point::getVelocity);
+		.function("getPos", (PointGetPos2)&Point::getPos)
+		.function("getVelocity", &Point::getVelocity)
+		.function("pathsTo", &Point::pathsTo);
 }
