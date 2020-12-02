078158adc7a48fed2523934b993976fafc0a8427
andyshafer <andyshafer0715@gmail.com>
2020-12-01 23:59:58 -0500

    Display shows start, end, and walls with time offset

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -6,14 +6,27 @@ const e = React.createElement;
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			env: {
+				start: {x: 50, y:50},
+				end: {x:350, y:250},
+				walls: [
+					{
+						id: 0,
+						point1: {x: 100, y: 50, vx: 0, vy: 10},
+						point2: {x: 100, y: 150, vx: 10, vy: 0}
+					}
+				],
+				time: 4
+			}
+		};
 	}
 
 	render() {
 		return (
 			<React.Fragment>
 				<div className="row"><div className="col"><ControlBar /></div></div>
-				<div className="row"><div className="col"><Display /></div></div>
+				<div className="row"><div className="col"><Display env={this.state.env}/></div></div>
 				<div className="row"><div className="col"><PlayBar /></div></div>
 			</React.Fragment>
 		);
--- web-app/src/display.jsx
+++ web-app/src/display.jsx
@@ -9,7 +9,13 @@ class Display extends React.Component {
 	render() {
 		 return (
 			<svg id="display" width="1080" height="600" style={{border: '3px solid #000000'}}>
-				<circle cx="50" cy="50" r="40" stroke="green" strokeWidth="4" fill="yellow" />
+				<circle cx={this.props.env.start.x} cy={this.props.env.start.y} r="10" stroke="black" strokeWidth="2" fill="green" />
+				<circle cx={this.props.env.end.x} cy={this.props.env.end.y} r="10" stroke="black" strokeWidth="2" fill="red" />
+			 	{ this.props.env.walls.map(wall => <line key={wall.id}
+					x1={wall.point1.x + this.props.env.time * wall.point1.vx}
+					y1={wall.point1.y + this.props.env.time * wall.point1.vy}
+					x2={wall.point2.x + this.props.env.time * wall.point2.vx}
+					y2={wall.point2.y + this.props.env.time * wall.point2.vy} stroke="black" strokeWidth="2" />) }
 			</svg>
 		 );
 	}
