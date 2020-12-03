8fd68a1227c95962dda78af14f39f92eec527ddc
andyshafer <andyshafer0715@gmail.com>
2020-12-02 19:24:41 -0500

    Button switches between play and pause when clicked

--- web-app/src/play_bar.jsx
+++ web-app/src/play_bar.jsx
@@ -3,14 +3,32 @@
 class PlayBar extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			paused: true
+		};
+	}
+
+	play = () => {
+		this.setState({ paused: false })
+	}
+
+	pause = () => {
+		this.setState({ paused: true })
+	}
+
+	getPlayOrPauseButton = () => {
+		if(this.state.paused) {
+			return <button type="button" onClick={this.play} className="btn btn-default btn-primary">Play</button>
+		} else {
+			return <button type="button" onClick={this.pause} className="btn btn-default btn-primary">Pause</button>
+		}
 	}
 
 	render() {
 		 return (
 			<div id="play-bar" style={{border: '3px solid #000000'}}>
 				<button type="button" className="btn btn-default btn-primary">Back</button>
-				<button type="button" className="btn btn-default btn-primary">Play</button>
+			 	{ this.getPlayOrPauseButton() }
 				<button type="button" className="btn btn-default btn-primary">Replay</button>
 			</div>
 		 );
