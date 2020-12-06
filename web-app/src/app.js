65ee5d07149b64fa5e5776a5db385eff3679e565
andyshafer <andyshafer0715@gmail.com>
2020-12-05 19:26:38 -0500

    Mode buttons indicate if they are active

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -189,7 +189,7 @@ class App extends React.Component {
 		return (
 			<div className="container" onKeyDown={this.onKeyDown}>
 				<div className="row"><div className="col">
-					<ControlBar env={this.state.env} setMode={this.setMode} selectedAttributes={this.getSelectedAttributes()} onInputChanged={this.onInputChanged}/>
+					<ControlBar env={this.state.env} mode={this.state.mode} setMode={this.setMode} selectedAttributes={this.getSelectedAttributes()} onInputChanged={this.onInputChanged}/>
 				</div></div>
 				<div className="row"><div className="col">
 					<Display env={this.state.env} path={this.state.path} mode={this.state.mode} selection={this.state.selection}
--- web-app/src/control_bar.jsx
+++ web-app/src/control_bar.jsx
@@ -33,18 +33,29 @@ class ControlBar extends React.Component {
 		); });
 	}
 
+	getButtonClasses = (mode) => {
+		var classes = "btn btn-default btn-secondary"
+		if(mode == "draw-wall" || mode == "pan") {
+			classes += " ml-2";
+		}
+		if(mode == this.props.mode) {
+			classes += " active";
+		}
+		return classes;
+	}
+
 	render() {
 		return (
 			<div id="control-bar" className="container mb-2 mt-2">
 				<div className="row">
 					<div className="col-md-2">
-						<button type="button" onClick={this.editClicked} className="btn btn-default btn-secondary" title="Edit">
+						<button type="button" onClick={this.editClicked} className={ this.getButtonClasses("edit") } title="Edit">
 							<i className="fas fa-edit"></i>
 						</button>
-						<button type="button" onClick={this.wallClicked} className="btn btn-default btn-secondary ml-2" title="Draw Wall">
+						<button type="button" onClick={this.wallClicked} className={ this.getButtonClasses("draw-wall") } title="Draw Wall">
 							<i className="fas fa-pencil-alt"></i>
 						</button>
-						<button type="button" onClick={this.panClicked} className="btn btn-default btn-secondary ml-2" title="Pan">
+						<button type="button" onClick={this.panClicked} className={ this.getButtonClasses("pan") } title="Pan">
 							<i className="fas fa-hand-paper"></i>
 						</button>
 					</div>
